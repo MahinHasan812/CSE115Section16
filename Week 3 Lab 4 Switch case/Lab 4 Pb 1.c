1a515cf336448181c2e99f49fad7dc1a6b1cc4d0
MahinHasan812 <68634077+MahinHasan812@users.noreply.github.com>
2020-08-02 16:04:34 +0600

    Add files via upload

---
+++ Week 3 Lab 4 Switch case/Lab 4 Pb 1.c
@@ -0,0 +1,31 @@
+// Write a program that takes an arithmetic operator ('+',  '-', '*'  or  '/')
+//and two operands as input and perform the corresponding calculation on the operands.
+
+#include <stdio.h>
+int main() {
+    char operator;
+    double first, second;
+    printf("Enter an operator (+, -, *,): ");
+    scanf("%c", &operator);
+    printf("Enter two operands: ");
+    scanf("%lf %lf", &first, &second);
+
+    switch (operator) {
+    case '+':
+        printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+        break;
+    case '-':
+        printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+        break;
+    case '*':
+        printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+        break;
+    case '/':
+        printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+        break;
+    default:
+        printf("Error! operator is not correct");
+    }
+
+    return 0;
+}
