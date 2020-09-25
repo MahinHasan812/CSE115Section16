4c81d2c964a450075324daedcf5e158c365e736e
MahinHasan812 <68634077+MahinHasan812@users.noreply.github.com>
2020-09-25 15:01:53 +0600

    Add files via upload

---
+++ Week 10 File/Examples/Lab 12 ex-2.c
@@ -0,0 +1,13 @@
+#include<stdio.h >
+#include<string.h>
+int main()
+{
+    FILE *fp;
+    char buffer[30];
+    strcpy(buffer,"File has been created");
+    fp=fopen("test.txt","w");
+    fprintf(fp,buffer);
+    fclose(fp);
+    return 0;
+}
+
