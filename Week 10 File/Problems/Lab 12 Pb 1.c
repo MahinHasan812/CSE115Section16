37aa12a3eb8b73089dcf5a39373c21ffabe902dc
MahinHasan812 <68634077+MahinHasan812@users.noreply.github.com>
2020-09-27 12:27:02 +0600

    Add files via upload

---
+++ Week 10 File/Problems/Lab 12 Pb 1.c
@@ -0,0 +1,52 @@
+//Write a program that will read names, ids, dept names, and cgpa
+//of some students from a file and will show the results. Consider
+//that the name of the file is ‘input.csv’. It is just a text file
+//where each line holds information of one student. Example format
+//of two lines in the file is as follows
+
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	typedef struct
+	{
+		char name[50];
+        int id;
+        char dept[20];
+        double cgpa;
+	}student;
+
+	student std[15];
+	FILE * flp;
+	int x = 0, i;
+	char s1[100], c;
+	char s2[100];
+	int n;
+	double y;
+
+	flp = fopen("input.txt", "r");
+	if(!flp)
+	{
+		printf("Unable to open file\n");
+		return ;
+	}
+
+	while((c=getc(flp))!=EOF){
+    putchar(c);
+	}
+	fclose(flp);
+
+    flp = fopen("input.txt", "r");
+	while (fscanf(flp,"%[^,],%d,%[^,],%lf",s1, &n, s2, &y) != EOF)
+	{
+
+		strcpy(std[x].name,s1);
+		std[x].id = n;
+		strcpy(std[x].dept,s2);
+		std[x].cgpa = y;
+		x++;
+	}
+	printf("\n");
+	fclose(flp);
+}
